526f55926917e86c5e31656dfa9c1f193c1cbd00
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-19 18:03:02 +0000

    installing flash from pip3 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# Installing flask from pip3
+# Install Flask version 2.1.0 using pip3 provider
+# This ensures that Flask is installed with the specific version required
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug version 2.1.1 using pip3 provider
+# This ensures that Werkzeug is installed with the specific version required
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
