2dcec3dacb9e10fcaed6e95e9fe095b0ce671295
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-19 17:33:38 +0000

    changed ensure attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Define a file resource for /tmp/school
 file { '/tmp/school':
-ensure => file,
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => "I love Puppet",
+	ensure => present,
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => "I love Puppet",
 }
