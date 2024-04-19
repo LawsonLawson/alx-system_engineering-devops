56ab4682d163dc3ca1893df598e7714571c752cb
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-19 17:42:24 +0000

    Created a file named 'school' in the /tmp directory using Puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
 # Define a file resource for /tmp/school
+
 file { '/tmp/school':
-	ensure => present,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => "I love Puppet",
-}
+  ensure  => 'present',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+    }
