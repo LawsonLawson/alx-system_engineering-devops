d5e98aa14a3e3f370500aa2158995bfa09856421
Lawson Israel Pascal <isreallawson@outlook.com>
2024-06-13 13:25:13 +0000

    puppet-lint fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,7 +5,7 @@ exec { 'OS security config':
   # This command uses 'sed' to replace occurrences of 'holberton' with 'foo'
   # in the /etc/security/limits.conf file.
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  
+
   # The 'path' attribute specifies the directories to include in the search path
   # for the command execution. This ensures that 'sed' is found and executed correctly.
   path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
